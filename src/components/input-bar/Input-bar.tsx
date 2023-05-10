b2e888b3757886fe368988efd89076bdf967f8a1
Uladzimir Kazantsau <gordiancrew@gmail.com>
2023-05-11 00:35:04 +0300

    refactor: text style

--- src/components/input-bar/Input-bar.tsx
+++ src/components/input-bar/Input-bar.tsx
@@ -45,14 +45,37 @@ function InputBar() {
   async function requestApi() {
     setAnswerField('Loading...');
 
-    axios
-      .post('https://fastapi-happyai.onrender.com/hello', {
+    const response = await fetch('https://fastapi-happyai.onrender.com/hello', {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify({
         link: currentValue,
         company: companyValue,
         button: radioValue,
-      })
-      .then((response) => response.data)
-      .then((res) => setAnswerField(res.output_text));
+      }), // body data type must match "Content-Type" header
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      setAnswerField(data.output_text);
+    }
+
+    // axios
+    //   .post('https://fastapi-happyai.onrender.com/hello', {
+    //     link: currentValue,
+    //     company: companyValue,
+    //     button: radioValue,
+    //   })
+    //   .then((response) => response.data)
+    //   .then((res) => setAnswerField(res.output_text));
 
     // https://www.linkedin.com/in/williamhgates/
   }
